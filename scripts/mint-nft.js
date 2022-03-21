89583385d3a3844a59d98b2ee1787c44a002b7f2
Ryan Graham <rg@ryangraham.ca>
2022-03-20 21:21:57 -0400

    made some changes to be able to mint the nft for someone else, but I don't see it going through... might be a timing thing...

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const API_URL = process.env.API_URL;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const DESTINATION_KEY = process.env.DESTINATION_KEY;
 
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
 const web3 = createAlchemyWeb3(API_URL);
@@ -19,7 +20,7 @@ async function mintNFT(tokenURI) {
     to: contractAddress,
     nonce: nonce,
     gas: 500000,
-    data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+    data: nftContract.methods.mintNFT(DESTINATION_KEY, tokenURI).encodeABI(),
   };
 
   const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
